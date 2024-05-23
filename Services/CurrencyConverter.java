017a1c40719a2107a0a0a7da5ae1559da53ceb50
Dtopiast <102709670+Dtopiast@users.noreply.github.com>
2024-05-22 23:04:50 -0600

    add documentation in service

--- Services/CurrencyConverter.java
+++ Services/CurrencyConverter.java
@@ -1,6 +1,12 @@
 package Services;
 
 public class CurrencyConverter implements  ICurrencyConverter{
+    /**
+     * @param from Currency origin
+     * @param to Currency to convert
+     * @param amount Amount to convert
+     * @retur converted amount
+     */
     @Override
     public float getConversion(Currencies from, Currencies to, float amount) {
         return 0;
--- Services/ICurrencyConverter.java
+++ Services/ICurrencyConverter.java
@@ -2,5 +2,11 @@
 package Services;
 
 public interface ICurrencyConverter {
+    /**
+     * @param from Currency origin
+     * @param to Currency to convert
+     * @param amount Amount to convert
+     * @retur converted amount
+     */
     float getConversion(Currencies from, Currencies to, float amount);
 }
